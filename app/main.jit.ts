22c423eac71531b07aeddfba6922dc94a8238368
Martin Mädler <martin.maedler@gmail.com>
2017-08-21 12:48:36 +0200

    Enable production mode

--- app/main.jit.ts
+++ app/main.jit.ts
@@ -1,5 +1,21 @@
-import 'reflect-metadata';
+import {disableDebugTools} from '@angular/platform-browser';
+import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// Angular debug tools in the dev console.
+let decorateModuleRef = <T>(value: T): T => value;
+
+enableProdMode();
+
+// Production
+decorateModuleRef = (modRef: any) => {
+  disableDebugTools();
+
+  return modRef;
+};
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then(decorateModuleRef)
+  .catch((message) => console.error(message));
