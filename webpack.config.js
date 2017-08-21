a5d502ba7f7e72734f1c8dd62ebd57a450bf505b
Martin Mädler <martin.maedler@gmail.com>
2017-08-21 13:01:32 +0200

    Added bundle size optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const ngToolsWebpack = require('@ngtools/webpack');
 const path = require("path");
 
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+
 module.exports = {
   resolve: {
     extensions: ['.ts', '.js']
@@ -15,6 +18,35 @@ module.exports = {
   plugins: [
     new ngToolsWebpack.AotPlugin({
       tsConfigPath: './tsconfig.aot.json'
+    }),
+    new OptimizeJsPlugin({sourceMap: false}),
+    new UglifyJsPlugin({
+      beautify: false,
+      output: {
+        comments: false
+      },
+      mangle: {
+        // eslint-disable-next-line camelcase
+        screw_ie8: true
+      },
+      compress: {
+        // eslint-disable-next-line camelcase
+        screw_ie8: true,
+        warnings: false,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        // eslint-disable-next-line camelcase
+        dead_code: true,
+        evaluate: true,
+        // eslint-disable-next-line camelcase
+        if_return: true,
+        // eslint-disable-next-line camelcase
+        join_vars: true,
+        // eslint-disable-next-line camelcase
+        negate_iife: false // We need this for lazy v8
+      }
     })
   ],
   module: {
